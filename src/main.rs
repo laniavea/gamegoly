3760c45c03d996a845de483b5cca252225828093
laniavea <laniavea@yahoo.com>
2024-07-14 20:56:56 +0300

    Field as a separate file

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,20 @@ fn main() -> Result<(), slint::PlatformError> {
     let field_data = config_field::read_config("./test_field.json").unwrap();
     let player_data = config_player::read_config("./player.json").unwrap();
 
-    main_window.set_field_top(field_data.field_top());
-    main_window.set_field_right(field_data.field_right());
-    main_window.set_field_left(field_data.field_left());
-    main_window.set_field_bottom(field_data.field_bottom());
-    main_window.set_number_of_tiles(field_data.field_number_of_elems());
+    main_window.global::<FieldAdapter>().set_field_top(field_data.field_top());
+    main_window.global::<FieldAdapter>().set_field_right(field_data.field_right());
+    main_window.global::<FieldAdapter>().set_field_left(field_data.field_left());
+    main_window.global::<FieldAdapter>().set_field_bottom(field_data.field_bottom());
+    main_window.global::<FieldAdapter>().set_number_of_tiles(field_data.field_number_of_elems());
 
-    main_window.set_main_info_title(field_data.main_info_title());
+    main_window.global::<FieldAdapter>().set_main_info_title(field_data.main_info_title());
 
-    main_window.set_player_loc_id(player_data.location());
+    main_window.global::<FieldAdapter>().set_player_loc_id(player_data.location());
 
     let (ver_state, hor_state) = utils::get_ver_hor_state(player_data.location(), field_data.field_number_of_elems());
 
-    main_window.set_player_on_ver(ver_state);
-    main_window.set_player_on_hor(hor_state);
+    main_window.global::<FieldAdapter>().set_player_on_ver(ver_state);
+    main_window.global::<FieldAdapter>().set_player_on_hor(hor_state);
 
     main_window.run()
 }
