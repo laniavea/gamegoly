2fc466d569a590ace384d793cf5a4005a33d0907
laniavea <laniavea@yahoo.com>
2025-09-22 22:06:02 +0300

    Config read template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use actix_web::{web, App, HttpResponse, HttpServer};
 
+mod game_config;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    game_config::read_config_field("./static/my_field.toml".to_string());
+
+    HttpServer::new(|| {
+        App::new()
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("/") }),
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
